41c5d298bd4c710762a25fb8f90db024dc843b88
Vinicius Dias Moia <vinidm98@gmail.com>
2026-05-08 00:01:34 -0300

    correção para deploy 1

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,5 +1,5 @@
 import  prisma  from "@/lib/prisma";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { LoginInput } from "@/lib/validators/auth";
 
 export async function registerUser(data: {
